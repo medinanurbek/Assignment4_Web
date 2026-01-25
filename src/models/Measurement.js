902699e245325995b197f85d986e4dd9bd3d93c9
Medina Nurbek <medinanurbek0606@mail.ru>
2026-01-26 02:20:22 +0500

    Initial commit: analytical platform with Node.js, MongoDB, Chart.js

---
+++ src/models/Measurement.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const measurementSchema = new mongoose.Schema({
+  timestamp: { type: Date, required: true },
+  field1: { type: Number, required: true },
+  field2: { type: Number, required: true },
+  field3: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Measurement', measurementSchema);
